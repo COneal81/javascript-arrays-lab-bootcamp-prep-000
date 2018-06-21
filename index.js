85389751a1a5f42ae04cf98e34aae73ba5387fd1
triadfirewood@gmail.com <triadfirewood@gmail.com>
2018-06-21 18:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,21 @@ function destructivelyAppendKitten() {
   return kittens
 }
 
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob");
+  return kittens
+}
+const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
 
+function destructivelyAppendKitten() {
+  kittens.push("Ralph");
+  return kittens
+}
+
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob");
+  return kittens
+}
 
